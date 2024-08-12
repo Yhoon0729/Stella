0a61cf7523c3b46a83a6f0f04edb102012f785b9
Kim YoungHoon <yh0729@kakao.com>
2024-08-12 16:56:28 +0900

    15번째 커밋
    .gitignore 추가

--- Stella/settings.py
+++ Stella/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -132,15 +134,17 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+# .env 파일 로드
+load_dotenv()
 
 # 이메일 서비스 Amazon ses
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'email-smtp.ap-northeast-2.amazonaws.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'AKIAR72PHSNYEB2EHJI5'
-EMAIL_HOST_PASSWORD = 'BGfQDxon0DkP2araoE6ofSAZyVX/9k+uwkFE4yXnpgXL'
-DEFAULT_FROM_EMAIL = 'vfdgy0729@gmail.com'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
 CACHES = {
     'default': {
